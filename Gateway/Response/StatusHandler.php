b9ef886c99b43418fbaf5eac6108275d9cc3655a
David Runtic <osdavidruntic@gmail.com>
2026-04-08 15:04:37 +0200

    moved out status handler out of google pay

--- Gateway/Response/StatusHandler.php
+++ Gateway/Response/StatusHandler.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * This file is part of the Monri Payments module
+ *
+ * (c) Monri Payments d.o.o.
+ *
+ * @author Favicode <contact@favicode.net>
+ */
 
-namespace Monri\Payments\Gateway\Response\GooglePay;
+namespace Monri\Payments\Gateway\Response;
 
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Payment\Gateway\Response\HandlerInterface;
@@ -10,11 +17,10 @@ use Magento\Sales\Model\Order\Payment;
 class StatusHandler implements HandlerInterface
 {
     /**
-     * Updates payment additional information with gateway response details.
+     * Updates payment additional information with gateway status response details.
      *
-     * @param array $handlingSubject The handling subject containing contextual information for processing.
-     * @param array $response The response data received from the payment gateway.
-     *
+     * @param array $handlingSubject
+     * @param array $response
      * @return void
      * @throws LocalizedException
      */
@@ -24,10 +30,11 @@ class StatusHandler implements HandlerInterface
         /** @var Payment $payment */
         $payment = $paymentDO->getPayment();
 
-        // Save gateway status info for use is payment handler
+        // Save gateway status info for use in payment handler
         $payment->setAdditionalInformation('gateway_status', $response['status'] ?? null);
         $payment->setAdditionalInformation('gateway_response_code', $response['response-code'] ?? null);
         $payment->setAdditionalInformation('gateway_transaction_type', $response['transaction-type'] ?? null);
         $payment->setAdditionalInformation('gateway_approval_code', $response['approval-code'] ?? null);
     }
 }
+
